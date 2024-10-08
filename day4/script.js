d8225623e776517dc87afcc54eefc1fa9aeb8af6
fjchx <vivekpandeyji24@gmail.com>
2024-10-08 15:26:17 +0530

    validation using js

---
+++ day4/script.js
@@ -0,0 +1,13 @@
+function validate(){
+    let username = document.getElementById("un").value;
+let passss = document.getElementById("pass").value;
+let pl=document.getElementById('result');
+if (username == "admin") 
+{
+    if (passss == "admin") {
+       p.innerText="yo man !!"}
+}
+else{
+    alert('invalid')
+}
+}
---
+++ day4/valid.js
@@ -0,0 +1,6 @@
+function validate(){
+ let username=document.getElementById("un").value;
+ let passss = document.getElementById("pass").value;
+if (username.length < 10){
+    return false;
+}}
